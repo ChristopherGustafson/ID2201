89380765410c9e63a0ba5afc9267a71ade04e52a
ChristopherGustafson <christopher.gustafson@outlook.com>
2020-09-08 08:51:57 +0200

    Finished dijkstra algorithm

---
+++ assignment2/dijkstra.erl
@@ -0,0 +1,50 @@
+-module(dijkstra).
+
+-export([table/2, route/2]).
+
+route(Node, Table) ->
+  case lists:keyfind(Node, 1, Table) of
+    {_, Gateway} ->
+      {ok, Gateway};
+    false ->
+      {error, notfound}
+    end.
+
+table(Gateways, Map) ->
+  InitialList = lists:map(fun(Node) -> {Node, inf, unkown} end, map:all_nodes(Map)),
+  Sorted = lists:foldl(fun(Gateway, Sorted) -> update(Gateway, 0, Gateway, Sorted) end, InitialList, Gateways),
+  iterate(Sorted, Map, []).
+
+
+iterate([], _, Table) ->
+  Table;
+iterate(_, [{_, inf, _} | _], Table) ->
+  Table;
+iterate([{Node, Length, Gateway} | Tail], Map, Table) ->
+  Reachable = map:reachable(Node, Map),
+  Rest = lists:foldl(fun(Elem, Sorted) -> update(Elem, Length+1, Node, Sorted) end, Tail, Reachable),
+  iterate(Rest, Map, Table ++ [{Node, Gateway}]).
+
+
+update(Node, N, Gateway, Sorted) ->
+  L = entry(Node, Sorted),
+  if 
+    N < L -> 
+      replace(Node, N, Gateway, Sorted);
+    true ->
+      Sorted
+    end.
+
+entry(_, []) ->
+  0;
+entry(Node, [{Dest, Length, _} | Tail]) ->
+  case Dest of
+    Node ->
+      Length;
+    _ ->
+      entry(Node, Tail)
+    end. 
+
+replace(Node, N, Gateway, Sorted) ->
+  Unsorted = lists:keyreplace(Node, 1, Sorted, {Node, N, Gateway}),
+  lists:keysort(2, Unsorted).
